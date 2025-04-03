fa71c4d1a177504ec7434a267788984ae41233af
soumeya56 <91370143+soumeya56@users.noreply.github.com>
2025-04-03 23:33:36 +0200

    Delete TestEnexse directory

--- TestEnexse/src/main/java/com/enexse/test/exceptions/ResourceNotFindException.java
+++
@@ -1,9 +0,0 @@
-package com.enexse.test.exceptions;
-
-public class ResourceNotFindException extends RuntimeException {
-private static final long serialVersionUID = 1L;
-	 
-	public ResourceNotFindException(String message) {
-		super(message);
-	}
-}
